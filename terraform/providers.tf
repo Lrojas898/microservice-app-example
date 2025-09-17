51596d4f7ce5fece8745b9bfc354754a5b4ca43b
Luis Manuel Rojas <luis.manuel.rojas71@gmail.com>
2025-09-17 12:13:13 -0500

    chore(terraform): bump azurerm/random providers and init with -upgrade to load flexible DB schemas

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,12 +1,13 @@
 terraform {
+  required_version = ">= 1.5.7"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "~> 3.113.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.1"
+      version = "~> 3.5"
     }
   }
 }
