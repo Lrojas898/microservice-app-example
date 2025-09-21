0c074e9bcbf14f60b407542f00237389e340f96e
Luis Manuel Rojas <luis.manuel.rojas71@gmail.com>
2025-09-20 19:48:42 -0500

    feat: comentario de preuba test para java

--- users-api/src/main/java/com/elgris/usersapi/api/UsersController.java
+++ users-api/src/main/java/com/elgris/usersapi/api/UsersController.java
@@ -1,15 +1,21 @@
 package com.elgris.usersapi.api;
 
-import com.elgris.usersapi.models.User;
-import com.elgris.usersapi.repository.UserRepository;
-import io.jsonwebtoken.Claims;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
 import java.util.LinkedList;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.elgris.usersapi.models.User;
+import com.elgris.usersapi.repository.UserRepository;
+
+import io.jsonwebtoken.Claims;
 
 @RestController()
 @RequestMapping("/users")
@@ -19,6 +25,7 @@ public class UsersController {
     private UserRepository userRepository;
 
 
+    //commentario de prueba pipeline correcto dev para java
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public List<User> getUsers() {
         List<User> response = new LinkedList<>();
