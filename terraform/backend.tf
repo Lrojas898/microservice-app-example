46888f68a6af031278d7c0c7a6b1ee1301b03225
Luis Manuel Rojas <66932083+Lrojas898@users.noreply.github.com>
2025-09-22 14:37:32 -0500

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "terraform-state-rg"
-    storage_account_name = "ingesofttstatestorage"
+    storage_account_name = "ingesoftvtstatestorage"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
   }
