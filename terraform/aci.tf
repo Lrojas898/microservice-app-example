d9021d4b7f568aa1385386d68c45c9d6e5df67cb
OscarGomez-jpg <os.gomez.lozano@gmail.com>
2025-09-23 14:53:18 -0500

    corrected urls

--- terraform/aci.tf
+++ terraform/aci.tf
@@ -177,11 +177,10 @@ resource "azurerm_container_group" "frontend" {
       protocol = "TCP"
     }
 
-    # CORRECCIÓN: Usar IP pública del Application Gateway con rutas correctas
+    # CORRECCIÓN: Usar IPs privadas de los containers directamente
     environment_variables = {
-      AUTH_API_ADDRESS  = "http://${data.azurerm_public_ip.appgw.ip_address}/api/auth"
-      TODOS_API_ADDRESS = "http://${data.azurerm_public_ip.appgw.ip_address}/api/todos"
-      USERS_API_ADDRESS = "http://${data.azurerm_public_ip.appgw.ip_address}/api/users"
+      AUTH_API_ADDRESS  = "http://${azurerm_container_group.auth.ip_address}:8000"
+      TODOS_API_ADDRESS = "http://${azurerm_container_group.todos.ip_address}:8082"
     }
   }
 
