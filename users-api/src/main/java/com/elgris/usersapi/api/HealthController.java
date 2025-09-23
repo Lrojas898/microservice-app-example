7b89e2e707696ce0b29d7c36c6cc3fc615649c30
Luis Manuel Rojas <luis.manuel.rojas71@gmail.com>
2025-09-23 17:07:26 -0500

    feat: users api update

--- users-api/src/main/java/com/elgris/usersapi/api/HealthController.java
+++ users-api/src/main/java/com/elgris/usersapi/api/HealthController.java
@@ -1,12 +1,12 @@
 package com.elgris.usersapi.api;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 public class HealthController {
@@ -20,4 +20,4 @@ public class HealthController {
 
         return new ResponseEntity<>(healthStatus, HttpStatus.OK);
     }
-}z
+}
